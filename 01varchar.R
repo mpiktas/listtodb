995add2c55fcbf0a7ba204d6f3ba027744541982
Dirk Eddelbuettel <edd@debian.org>
2012-07-26 22:19:11 -0500

    some trials around 01varchar -- still unsuccessful much simpler 'proof of pudding' example in 02simpler.R

--- 01varchar.R
+++ 01varchar.R
@@ -1,4 +1,5 @@
-rm(list=ls())
+
+## That is generally considered a bad idea...  rm(list=ls())
 source("code/simulate.R")
 library(RSQLite)
 library(forecast)
@@ -6,17 +7,19 @@ library(forecast)
 
 set.seed(1313)
 
-id1 <- unique(sapply(1:30,function(i)paste0(sample(toupper(letters),5,replace=TRUE),collapse="")))
+id1 <- unique(sapply(1:30, function(i) paste0(sample(toupper(letters),5,replace=TRUE),collapse="")))
 
-id2 <- unique(sapply(1:30,function(i)paste0(sample(letters,5,replace=TRUE),collapse="")))
+id2 <- unique(sapply(1:30, function(i) paste0(sample(letters,5,replace=TRUE),collapse="")))
 
-llen <- 10
+##llen <- 10
+llen <- 1
 
-objdata <- lapply(rep(200,llen),asim)
+#objdata <- lapply(rep(200,llen),asim)
+objdata <- lapply(rep(2,llen),asim)
 
-objlist <- lapply(objdata,function(x)list(data=x,model=auto.arima(x)))
+objlist <- lapply(objdata, function(x) list(data=x,model=auto.arima(x)))
 
-o1 <- mapply(function(x,id1,id2)c(list(id1=id1,id2=id2),x),objlist,sample(id1,llen,replace=TRUE),sample(id2,llen,replace=TRUE),SIMPLIFY=FALSE)
+o1 <- mapply(function(x,id1,id2) c(list(id1=id1,id2=id2),x),objlist,sample(id1,llen,replace=TRUE),sample(id2,llen,replace=TRUE),SIMPLIFY=FALSE)
 
 o2 <- lapply(o1,function(x){
     x$data <- rawToChar(serialize(x$data,NULL,ascii=TRUE))
@@ -28,12 +31,13 @@ o3 <- lapply(o2,function(x){
     x
 })
 
+## do we really need it as a data.frame ?
 todb <- as.data.frame(do.call("rbind",o3))
 
 m <- dbDriver("SQLite")
 
 con <- dbConnect(m, dbname = "smallvarchar.sqlite")
 
-dbWriteTable(con,"modeldata",data.frame(todb))
+dbWriteTable(con,"modeldata",todb)
 
 dbDisconnect(con)
---
+++ 02simpler.R
@@ -0,0 +1,27 @@
+
+library(stats)
+library(RSQLite)
+
+data(trees)
+
+fit <- lm(log(Volume) ~ log(Girth) + log(Height), data=trees)
+
+## test for serialization
+all.equal(fit,  unserialize( serialize(fit, NULL, ascii=TRUE) ) )
+
+m <- dbDriver("SQLite")
+con <- dbConnect(m, dbname = "/tmp/02simpler.sqlite")  ## adjust path if on Windows and /tmp does not exist
+if (!dbExistsTable(con, "foo"))
+    dbSendQuery(con, "create table foo (id integer, val varchar)")
+txt <-  paste("insert into foo values(1, \"", rawToChar(serialize(fit,NULL,ascii=TRUE)), "\")", sep="")
+dbSendQuery(con, txt)
+dbDisconnect(con)
+
+
+con <- dbConnect(m, dbname = "/tmp/02simpler.sqlite")  ## adjust path if on Windows and /tmp does not exist
+data <- dbGetQuery(con, "select * from foo where id==1")
+dbDisconnect(con)
+
+
+## new check
+all.equal(fit,  unserialize( charToRaw(data[,"val"]) ) )
